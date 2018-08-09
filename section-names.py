ccb230774ac68eb2a96c78fa9336695444ab0bbb
Tsukasa OI <floss_seccamp2018@irq.a4lg.com>
2018-08-09 11:55:06 +0900

    Answer of prior learning #02

--- section-names.py
+++ section-names.py
@@ -13,7 +13,11 @@ def section_name(e, idx):
 	#      先頭のセクションヘッダ (section_headers[0]) の sh_link メンバーの値
 	#  (2) 最終的なインデックスが 0 の場合、セクション名は定義できない
 	#      (この課題では、仮に b'' を返すこと)
-	raise NotImplementedError('課題実装用のテンプレートです。')
+	if ndx == SHN_XINDEX:
+		ndx = e.section_headers[0].sh_link
+	if ndx == 0:
+		return b''
+	return e.read_string_by_offset(e.section_headers[ndx].sh_offset + e.section_headers[idx].sh_name)
 
 for i in range(len(e.section_headers)):
 	print(section_name(e, i).decode('utf-8'))
