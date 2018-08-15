085fdac75a1dda52fc44716c209dd307ff7488a2
Tsukasa OI <floss_seccamp2018@irq.a4lg.com>
2018-08-15 11:30:48 +0900

    initial commit

---
+++ string-table.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+from z2kit2.elf import *
+from z2kit2.elffile import *
+import sys
+
+f = open('hello1', 'rb')
+e = ELFFile(f)
+
+if DT_STRTAB not in e.dynamic_headers:
+	sys.exit(1)
+if DT_STRSZ not in e.dynamic_headers:
+	sys.exit(1)
